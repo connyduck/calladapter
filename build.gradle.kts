1d85281ec1d75ebb2b29582765be94ce6eec68bb
Konrad Pozniak <k.pozniak@gmx.at>
2022-01-30 18:55:29 +0100

    set up MavenCentral publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,12 @@ buildscript {
     dependencies {
         classpath(kotlin("gradle-plugin", version = "1.6.10"))
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
+        classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
     }
 }
+
+apply(plugin = "io.github.gradle-nexus.publish-plugin")
 
 allprojects {
     repositories {
@@ -18,3 +22,5 @@ allprojects {
 tasks.register("clean", Delete::class.java) {
     delete(rootProject.buildDir)
 }
+
+apply(from= "${rootDir}/scripts/publish-root.gradle")
--- calladapter/build.gradle.kts
+++ calladapter/build.gradle.kts
@@ -3,18 +3,70 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
     `maven-publish`
+    signing
+    id("org.jetbrains.dokka")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "at.connyduck"
-            artifactId = "kotlin-result-calladapter"
-            version = "1.0.0"
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
 
-            from(components["java"])
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.dokkaJavadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc.get().outputDirectory.get())
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "at.connyduck"
+                artifactId = "kotlin-result-calladapter"
+                version = "1.0.0"
+
+                from(components["java"])
+                artifact(sourcesJar.get())
+                artifact(javadocJar.get())
+
+                pom {
+                    name.set("kotlin-result-calladapter")
+                    description.set("A Retrofit calladapter that allows to have Kotlin's Result as a return type")
+                    url.set("https://github.com/connyduck/calladapter")
+
+                    licenses {
+                        license {
+                            name.set("Apache-2.0 License")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("connyduck")
+                            name.set("Konrad Pozniak")
+                            email.set("opensource@connyduck.at")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:github.com/connyduck/calladapter.git")
+                        developerConnection.set("scm:git:ssh://github.com/connyduck/calladapter.git")
+                        url.set("https://github.com/connyduck/calladapter/tree/main")
+                    }
+                }
+            }
         }
     }
+}
+
+signing {
+    val signingKeyId = rootProject.ext["signing.keyId"] as String?
+    val signingKey = rootProject.ext["signing.key"] as String?
+    val signingPassword = rootProject.ext["signing.password"] as String?
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications)
 }
 
 val compileKotlin: KotlinCompile by tasks
